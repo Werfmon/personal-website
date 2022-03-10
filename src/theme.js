e7bee1af40061ec8a5284dc321bbd03eb91c5373
Werfmon <d.vyroubal.w@gmail.com>
2022-03-10 12:15:22 +0100

    idk

--- src/theme.js
+++ src/theme.js
@@ -4,4 +4,11 @@ export const Container = styled.div`
   top: 0;
   width: 100%;
   z-index: 10;
+  position: absolute;
 `;
+export const Paragraph = styled.p`
+  width: 50%;
+  margin: 0 auto;
+  color: #fff;
+  margin-top: 10rem;
+`
